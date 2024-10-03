3c5efd0bbac137d72cdd40fa3e2f13283c248a2b
Pj Elliott <pjelliott11@gmail.com>
2024-10-02 21:04:14 -0500

    Day 2 Challenge, Create a function for Candy Per Cost

--- October.js
+++ October.js
@@ -1,2 +1,10 @@
+/*Day 1
 message = "Happy Fall!";
-console.log(message);
+console.log(message); */
+//Day 2 Candy per cost calculator
+
+function howMuchCandy(myMoney, cost){
+let quantity = Math.round(myMoney/cost)
+return quantity + " candy bars is how much you can buy with $" + myMoney
+}
+console.log(howMuchCandy(10, 1.25));
