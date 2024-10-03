23eb7a02f02817fdc0d5dacd70201c93d7b6f96e
Pj Elliott <pjelliott11@gmail.com>
2024-10-03 14:15:00 -0500

    Day 3 Challenge, Create a loop for basic countdown until Halloween

--- October.js
+++ October.js
@@ -8,3 +8,12 @@ let quantity = Math.round(myMoney/cost)
 return quantity + " candy bars is how much you can buy with $" + myMoney
 }
 console.log(howMuchCandy(10, 1.25));
+//Day 3, it just wanted a basic loop, but I used a function to make a simple countdown until Halloween
+function countdown(number){
+     for(let i=number; i>= 0; i--){
+          console.log(i + " days til Halloween!")}
+ }
+
+
+
+countdown(10);
