b0d27a7b866a1e5678b20ba506353b7f3dcc2d58
Pj Elliott <pjelliott11@gmail.com>
2024-10-07 23:33:43 -0500

    Day 7 Challenge, Created a candyTracker.js and Updated Day 6 HTML to accept input from it, candyTracker.html now takes input and displays it on the screen.

---
+++ candyTracker.js
@@ -0,0 +1,6 @@
+let favCandy = prompt(" What is your favorite candy?");
+
+let displayElement = document.getElementById("candyDisplay");
+
+displayElement.textContent = "Your favorite candy is " + favCandy + "!";
+
