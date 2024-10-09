c1d259ae87ed73e68496731e97a511d44ae88c08
Pj Elliott <pjelliott11@gmail.com>
2024-10-09 11:58:21 -0500

    Updated Day 8 Challenge, I had to learn the new way to create a React app with Vite. So I reset up my app, and it's finally displaying the message correctly.

---
+++ halloween-app/src/Message.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HalloweenMessage() {
+   return (
+      <div>
+         <h1>Happy Halloween!</h1>
+      </div>
+   );
+}
+
+export default HalloweenMessage;
