5dac51b337fccf43894f4b5fad0781aa1ffbc2e1
Pj Elliott <pjelliott11@gmail.com>
2024-10-08 23:30:53 -0500

    Day 8 Challenge, Created a react app "happy-halloween", still working on the component to display a message

---
+++ happy-halloween/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Message from './Message';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Spooky Message Below:</h1>
+            <Message />
+        </div>
+    );
+};
+
+export default App;
---
+++ happy-halloween/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Message = () => {
+    return (
+        <div>
+            <h1>Happy Halloween</h1>
+        </div>
+    );
+};
+
+export default Message;
