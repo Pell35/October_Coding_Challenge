4d5ffad5f13a51fa1b6e51b2da9f8ea39e6260f9
Pell35 <pjelliott11@gmail.com>
2024-10-13 23:49:20 -0500

    Updated Day 9 Challenge, created an array to store favorite candy

--- candyTracker.js
+++ candyTracker.js
@@ -1,5 +1,9 @@
-let favCandy = prompt(" What is your favorite candy?");
+let favCandy =[];
 
+ let candy = prompt(" What is your favorite candy?");
+if (candy){
+favCandy.push(candy);
+}
 let displayElement = document.getElementById("candyDisplay");
 
 displayElement.textContent = "Your favorite candy is " + favCandy + "!";
